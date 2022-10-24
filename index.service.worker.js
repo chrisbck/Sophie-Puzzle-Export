be2395182e805b1c15be906fe1d18ea068e626b6
DESKTOP-SB94ILT\dopey <dopeyphuck@hotmail.com>
2022-10-24 15:17:29 +0800

    theme changes and hard difficulty made harder

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1666422739|780058669";
+const CACHE_VERSION = "1666595773|2253221422";
 const CACHE_PREFIX = "Sliding Puzzle-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
